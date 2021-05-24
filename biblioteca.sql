de5c51e8c90b0b8da7c47ca0be3ab88e6e4f9f26
María Paz C <mpcarrascoojeda@gmail.com>
2021-05-24 17:50:37 -0400

    ALTER TABLE para agregar foreign key, editar nombre de etiquetas segun archivos copiados csv

--- biblioteca.sql
+++ biblioteca.sql
@@ -14,27 +14,37 @@ telefono VARCHAR UNIQUE
 );
 --crear tabla libros
 CREATE TABLE libros(
-ISBN VARCHAR(15) PRIMARY KEY,
+isbn VARCHAR(15) PRIMARY KEY,
 titulo VARCHAR,
-cod_autor VARCHAR,
 paginas VARCHAR,
+dias_prestamo INTEGER
+);
+--crear tabla autor
+CREATE TABLE autor(
+cod_autor INTEGER PRIMARY KEY,
+id_libro VARCHAR,
 nombre_autor VARCHAR,
 apellido_autor VARCHAR,
-nacimiento_muerte DATE,
-tipo_autor VARCHAR,
-dias_prestamo INTEGER
+nacimiento DATE,
+muerte DATE,
+tipo_autor VARCHAR
 );
 --crear tabla historial prestamos
 CREATE TABLE prestamos(
 id_prestamo SERIAL PRIMARY KEY,
-id_socio INTEGER NOT NULL REFERENCES socios(id),
-id_libro VARCHAR REFERENCES libros(ISBN),
+id_socio INTEGER NOT NULL,
+id_libro VARCHAR,
 socio VARCHAR (255),
 libro VARCHAR (255),
-fecha_prestamos DATE,
+fecha_prestamo DATE,
 fecha_devolucion DATE
 );
+ALTER TABLE autor ADD foreign key (id_libro) references libros(isbn);
+ALTER TABLE prestamos ADD foreign key (id_socio) references socios(id);
+ALTER TABLE prestamos ADD foreign key (id_libro) references libros(isbn);
+
 --inserción de registros
 \copy socios(rut, nombre, apellido, direccion, telefono) FROM '/Users/mariapazcarrascoojeda/Desktop/basesdatos/prueba/socios.csv' csv header;
-\copy libros(ISBN,titulo,paginas,cod_autor,nombre_autor,apellido_autor,nacimiento_muerte,tipo_autor,dias_prestamo) FROM '/Users/mariapazcarrascoojeda/Desktop/basesdatos/prueba/libros.csv' csv header;
-\copy prestamos(socio,libro,fecha_prestamos,fecha_devolucion) FROM '/Users/mariapazcarrascoojeda/Desktop/basesdatos/prueba/historialprestamos.csv' csv header;
+\copy libros(isbn,titulo,paginas,dias_prestamo) FROM '/Users/mariapazcarrascoojeda/Desktop/basesdatos/prueba/libros.csv' csv header;
+\copy autor(cod_autor,id_libro,nombre_autor,apellido_autor,nacimiento,muerte,tipo_autor) FROM '/Users/mariapazcarrascoojeda/Desktop/basesdatos/prueba/autor.csv' csv header;
+\copy prestamos(id_socio,id_libro,socio,libro,fecha_prestamo,fecha_devolucion) FROM '/Users/mariapazcarrascoojeda/Desktop/basesdatos/prueba/historialprestamos.csv' csv header;
