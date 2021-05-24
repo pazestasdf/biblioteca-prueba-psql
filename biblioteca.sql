88010dfa978d452909e92240b2be49c4a42a4047
María Paz C <mpcarrascoojeda@gmail.com>
2021-05-24 16:16:09 -0400

    id_libro no era INTEGER si no VARCHAR, libros.csv no tenia numero de paginas y socios no tenia un numero de telefono

--- biblioteca.sql
+++ biblioteca.sql
@@ -5,7 +5,7 @@ CREATE DATABASE biblioteca;
 --crear tabla socios
 CREATE TABLE socios(
 id SERIAL PRIMARY KEY, 
-rut, 
+rut VARCHAR(255) NOT NULL, 
 nombre VARCHAR, 
 apellido VARCHAR, 
 direccion VARCHAR NOT NULL UNIQUE, 
@@ -16,23 +16,24 @@ CREATE TABLE libros(
 ISBN VARCHAR(13) PRIMARY KEY,
 titulo VARCHAR,
 cod_autor VARCHAR,
+paginas VARCHAR,
 nombre_autor VARCHAR,
 apellido_autor VARCHAR,
 nacimiento_muerte DATE,
 tipo_autor VARCHAR,
-dias_prestamo INTEGER,
-)
+dias_prestamo INTEGER
+);
 --crear tabla historial prestamos
 CREATE TABLE prestamos(
 id_prestamo SERIAL PRIMARY KEY,
 id_socio INTEGER NOT NULL REFERENCES socios(id),
-id_libro INTEGER NOT NULL REFERENCES libros (ISBN),
+id_libro VARCHAR REFERENCES libros(ISBN),
 socio VARCHAR (255),
 libro VARCHAR (255),
 fecha_prestamos DATE,
-fecha_devolucion DATE,
-)
+fecha_devolucion DATE
+);
 --inserción de registros
-\copy socios(rut, nombre, apellido, direccion, telefono) from '/Users/mariapazcarrascoojeda/Desktop/basesdatos/prueba/socios.csv' csv header;
-\copy libros(ISBN,titulo,cod_autor,nombre_autor,apellido_autor,nacimiento_muerte,tipo_autor,dias_prestamo) from '/Users/mariapazcarrascoojeda/Desktop/basesdatos/prueba/libros.csv' csv header;
-\copy prestamos(socio,libro,fecha_prestamos,fecha_devolucion) from '/Users/mariapazcarrascoojeda/Desktop/basesdatos/prueba/historialprestamos.csv' csv header;
+\copy socios(rut, nombre, apellido, direccion, telefono) FROM '/Users/mariapazcarrascoojeda/Desktop/basesdatos/prueba/socios.csv' csv header;
+\copy libros(ISBN,titulo,paginas,cod_autor,nombre_autor,apellido_autor,nacimiento_muerte,tipo_autor,dias_prestamo) FROM '/Users/mariapazcarrascoojeda/Desktop/basesdatos/prueba/libros.csv' csv header;
+\copy prestamos(socio,libro,fecha_prestamos,fecha_devolucion) FROM '/Users/mariapazcarrascoojeda/Desktop/basesdatos/prueba/historialprestamos.csv' csv header;
